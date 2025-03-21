c936b0b3c43c94521d38fae0cf962d0bdad45de6
Erick Del Piero <Erick.gozales.r@uni.pe>
2025-03-21 13:38:34 -0500

    Primer html y css (0321V0)

---
+++ DEV_NOTES.md
@@ -0,0 +1,3 @@
+Note (0321V0)
+- Tengo dos imágenes una (jpeg y jpg).
+- Idea base es crear otros menú donde corran los modelos.
--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 import http.server
 import socketserver
+import os
 
 # Configurar el puerto donde se ejecutará el servidor
 PORT = 8000
 
-# Definir el manejador de solicitudes HTTP
-Handler = http.server.SimpleHTTPRequestHandler
+# Definir el directorio donde se encuentran los archivos a servir
+DIRECTORY = "frontend"
+
+# Clase personalizada para manejar las solicitudes HTTP
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+# Cambiar el directorio de trabajo actual al directorio donde está server.py
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Crear el servidor usando el manejador y el puerto especificado
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
     print(f"Servidor corriendo en http://localhost:{PORT}")
-    
     # Mantener el servidor en ejecución hasta que se detenga manualmente
     httpd.serve_forever()
